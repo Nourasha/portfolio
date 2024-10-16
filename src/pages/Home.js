6b2bcc8c2f8a1b9f9bcf9989a3bd2708a3bd27da
Nourasha <69981085+Nourasha@users.noreply.github.com>
2024-10-16 13:35:40 +0200

    //Some changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import sanityClient from "../lib/client";
 import BlockText from "../components/BlockContent";
 import Title from "../components/Title";
 import HeaderContent from "../components/HeaderContent";
+import Image from "../images/arrow.png"
+import Project from "./Project";
 
 export default function Home() {
   const [mainPost, setMainPost] = useState(null);
@@ -39,29 +41,33 @@ export default function Home() {
   return (
     <>
     <HeaderContent />
-    <main className="flex justify-between items-start min-h-screen w-auto">
+    <main className="flex flex-col justify-between items-start h-auto w-auto">
+    <span className="md:text-6xl text-black font-bold cursive mx-6 mb-4">
+              <h1>My Background</h1>
+            </span>
       {mainPost.map((post) => (
         <div key={post._id} className="container mx-auto md:flex md:justify-between md:items-center">
           <section className="mobile-flex">
-            <span className="text-6xl text-black font-bold cursive mx-6 mb-4">
-              <Title title={post.title} />
-            </span>
-            <div className="ml-6 text-blck flex">
-              <BlockText blocks={post.body} />
-            </div>
-          </section>
+
+            <div className="ml-6 pt-6 text-blck flex flex-row-reverse justify-start items-start">
+              <div className="prose prose-h1:text-red-600 pt-11 md:pl-3 prose-h3:text-black items-start ">
+              <BlockText blocks={post.body} className="" />
+              </div>
           <section>
             {post.mainImage?.asset && (
               <img
                 src={post.mainImage.asset.url}
                 alt={post.title}
-                className="img-mobile rounded-full h-60 w-60 my-6 mr-10"
+                className="h-20 w-20"
               />
             )}
+          </section>
+          </div>
           </section>
         </div>
       ))}
     </main>
+    <Project/>
     </>
   );
 }
