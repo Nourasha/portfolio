952c4f7450f373b592da85c6727868769c8f7861
Nourasha <69981085+Nourasha@users.noreply.github.com>
2024-10-20 12:23:23 +0200

    //

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -5,7 +5,7 @@ import MyImage from '../images/Bilde2.png';
 export default function HeaderContent() {
   return (
     <div className="flex flex-col justify-center md:flex md:flex-row bg-zinc-900 md:items-center md:justify-items-end h-screen overflow-hidden">
-    <span className="size text-xl md:text-6xl text-white font-bold delius-regular mx-5">
+    <span className="size text-xl md:text-6xl text-white font-bold gamja-flower-regular mx-5">
       <Title title={"Hello world. I'm Nour"} />
     </span>
     <img src={MyImage} alt="Nour's picture" className="invisible md:visible md:header-img md:rounded-sm object-cover" />
