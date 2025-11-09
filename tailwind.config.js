f2d41bfac52e7d3026d056bf62046d1c522d43fc
Nourasha <69981085+Nourasha@users.noreply.github.com>
2025-11-09 16:36:27 +0100

    New version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    // colors: {
-    //   transparent: 'transparent',
-    //   current: 'currentColor',
-      // 'offwhite': '#e0e1dd'
-    // },
   },
-  corePlugins: {preflight:false,},
-  plugins: [require('@tailwindcss/typography'),],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
