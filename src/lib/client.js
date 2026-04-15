6ca42f4ea767bba5256b150939041c637094a9fe
Nourasha <69981085+Nourasha@users.noreply.github.com>
2026-04-15 20:34:36 +0200

    Changed client

--- src/lib/client.js
+++ src/lib/client.js
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client';
 export default createClient({
   projectId: 'jw5kec04',
   dataset: 'production',
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2024-01-01',
 });
