75c621d9fd3034f0ee4389f258caf12f476309dc
Nourasha <69981085+Nourasha@users.noreply.github.com>
2026-04-15 19:02:24 +0200

    Changing the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,51 +1,51 @@
-import React, {useEffect, useState} from "react";
-import sanityClient from "../lib/client"
-import BlockText from "../components/BlockContent";
-import Title from "../components/Title";
+// import React, {useEffect, useState} from "react";
+// import sanityClient from "../lib/client"
+// import BlockText from "../components/BlockContent";
+// import Title from "../components/Title";
 
 
-export default function Home() {
-  const [mainPost, setMainPost] = useState(null);
+// export default function Home() {
+//   const [mainPost, setMainPost] = useState(null);
 
-  useEffect(() => {
-    sanityClient
-      .fetch(`*[_type == "mainPost"]{
-        title,
-        _id,
-        slug,
-        mainImage{
-          asset->{
-            _id,
-            url
-          }
-        },
-        body,
-      }`)
-      .then((data) => setMainPost(data))
-      .catch(console.error);
-  }, []);
+//   useEffect(() => {
+//     sanityClient
+//       .fetch(`*[_type == "mainPost"]{
+//         title,
+//         _id,
+//         slug,
+//         mainImage{
+//           asset->{
+//             _id,
+//             url
+//           }
+//         },
+//         body,
+//       }`)
+//       .then((data) => setMainPost(data))
+//       .catch(console.error);
+//   }, []);
 
-  // if (!mainPost) return <div>Loading...</div>;
+//   if (!mainPost) return <div>Loading...</div>;
 
-  return (
-    <main className="flex-col justify-between items-center min-h-screen w-auto">
-      {mainPost && mainPost.map((post, index) =>(
-          <div className="container mx-auto md:flex md:justify-between md:items-center">
-            <section className="mobile-flex">
-              <span className="text-6xl text-green-300 font-bold cursive mx-6 mb-4">
-                <Title title={post.title} />
-              </span>
-              <div className="ml-6 text-cyan-800">
-                <BlockText blocks={post.body} />
-              </div>
-            </section>
-            <section>
-            {post.mainImage?.asset && ( // Check if mainImage and asset exist
-                <img src={post.mainImage.asset.url} alt={post.title} className="img-mobile rounded-full h-60 w-60 my-6 mr-10" />
-            )}
-        </section>
-          </div>
-      ))}
-    </main>
-  )
-}
+//   return (
+//     <main className="flex-col justify-between items-center min-h-screen w-auto">
+//       {mainPost && mainPost.map((post, index) =>(
+//           <div className="container mx-auto md:flex md:justify-between md:items-center">
+//             <section className="mobile-flex">
+//               <span className="text-6xl text-green-300 font-bold cursive mx-6 mb-4">
+//                 <Title title={post.title} />
+//               </span>
+//               <div className="ml-6 text-cyan-800">
+//                 <BlockText blocks={post.body} />
+//               </div>
+//             </section>
+//             <section>
+//             {post.mainImage?.asset && ( // Check if mainImage and asset exist
+//                 <img src={post.mainImage.asset.url} alt={post.title} className="img-mobile rounded-full h-60 w-60 my-6 mr-10" />
+//             )}
+//         </section>
+//           </div>
+//       ))}
+//     </main>
+//   )
+// }
