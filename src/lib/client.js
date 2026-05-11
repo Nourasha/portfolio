b593033f778b60527040bfff4c71a1c9afb42ea1
Nourasha <69981085+Nourasha@users.noreply.github.com>
2026-05-11 16:54:44 +0200

    Fixed client

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export default createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || 'jw5kec04',
   dataset: 'production',
   useCdn: false,
   apiVersion: '2024-01-01',
